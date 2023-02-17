57da51ebc7e8ce08b5ccb183c27450cda2cfa856
Firmnaadi <yusuf.firman.12.06@gmail.com>
2023-02-17 09:11:03 +0700

    minggu2

---
+++ minggu2/app/src/main/java/com/example/minggu2/ConstraintActivity.java
@@ -0,0 +1,4 @@
+package com.example.minggu2;
+
+public class ConstraintActivity {
+}
---
+++ minggu2/app/src/main/java/com/example/minggu2/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.minggu2;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.Toast;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_linear_layout);
+    }
+}
